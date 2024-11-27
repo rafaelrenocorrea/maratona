fe3e0df5bea40070cfb58cebd310bb6849145059
Rafael Renó Corrêa <159826640+rafaelrenocorrea@users.noreply.github.com>
2024-11-27 15:26:01 -0300

    Delete Problemas resolvidos directory

--- Problemas resolvidos/Lista 2/1026 - Carrega ou não Carrega/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-  unsigned long long int a, b; // guarda binário como unsigned
-  
-  while(scanf("%lld %lld", &a, &b) != EOF)printf("%lld\n", a ^ b); // "a"  ou "b" bit a bit
-  
-  return 0;
-}
--- Problemas resolvidos/Lista 3/3344 - Brute/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    printf("4\n");
-
-    return 0;
-}
